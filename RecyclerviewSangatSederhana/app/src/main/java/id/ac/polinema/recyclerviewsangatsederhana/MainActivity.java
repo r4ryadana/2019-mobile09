19c7a85c4d6925516b9409dcd7738c68b1b8502c
aryadana <rizaldi220399@gmail.com.com>
2019-10-29 12:07:47 +0700

    tugas mobile9

--- RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/MainActivity.java
+++ RecyclerviewSangatSederhana/app/src/main/java/id/ac/polinema/recyclerviewsangatsederhana/MainActivity.java
@@ -1,14 +1,35 @@
 package id.ac.polinema.recyclerviewsangatsederhana;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import id.ac.polinema.recyclerviewsangatsederhana.adapters.SuperHeroAdapter;
+import id.ac.polinema.recyclerviewsangatsederhana.models.SuperHero;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvSuperHero;
+    List<SuperHero> superHeroList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvSuperHero = findViewById(R.id.rvSuperHero);
+
+        for (int i=0;i<=17;i++){
+            superHeroList.add(new SuperHero("-","RIZALDI"));
+        }
+        SuperHeroAdapter superHeroAdapter = new SuperHeroAdapter(getApplication(),superHeroList);
+        rvSuperHero.setAdapter(superHeroAdapter);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2, LinearLayoutManager.HORIZONTAL, false);
+        rvSuperHero.setLayoutManager(layoutManager);
     }
 }
